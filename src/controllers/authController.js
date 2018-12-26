7741929e236346d32d6991eda03a2c64bb312323
Baltazar Parra <btparra@esapiens.com.br>
2018-12-26 18:43:06 -0200

    auth user

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,5 +1,5 @@
 const express =  require('express')
-
+const bcrypt = require('bcryptjs')
 const User = require('../models/user')
 
 const router = express.Router()
@@ -18,6 +18,18 @@ router.post('/register', async (req, res) => {
     } catch (err) {
         return res.status(400).send({ error: 'Regster fail'})
     }
+})
+
+router.post('/authenticate', async (req, res) => {
+    const { email, password } = req.body
+    const user = await User.findOne({ email }).select('+password')
+    if (!user)
+        return res.status(400).send({error: 'User not found'})
+
+    if (!await bcrypt.compare(password, user.password))
+        return res.status(400).send({error: 'Invalid password'})
+
+    res.send({user})
 })
 
 module.exports = app => app.use('/auth', router)
