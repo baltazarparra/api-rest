842abd5315bce609e5fa3eed9b9e73765238aec4
Baltazar Parra <btparra@esapiens.com.br>
2019-01-07 05:30:40 -0200

    Change architecture

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -2,7 +2,7 @@ const express =  require('express')
 const bcrypt = require('bcryptjs')
 const jwt = require('jsonwebtoken')
 const User = require('../models/user')
-const authConfig = require('../config/auth')
+const authConfig = require('../../config/auth')
 const router = express.Router()
 
 function generateToken(params = {}) {
